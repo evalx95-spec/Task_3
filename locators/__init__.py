d399977d06f29a997cbd23bcea0a9b6e4aa7af01
Evgeniya Kozlova <evalx95@gmail.com>
2026-08-08 09:51:10 +0400

    Исправления task_3

--- locators/__init__.py
+++ locators/__init__.py
@@ -1,6 +1,3 @@
-﻿"""
-Пакет с локаторами для всех страниц.
-"""
 from .common_locators import CommonLocators
 from .auth_page_lct import AuthPageLocators
 from .main_page_lct import MainPageLocators
@@ -10,13 +7,14 @@ from .recovery_page_lct import RecoveryPageLocators
 
 
 class AllLocators:
-    """Класс-агрегатор всех локаторов."""
+    
     common = CommonLocators
     auth = AuthPageLocators
     main = MainPageLocators
     feed = FeedPageLocators
     profile = ProfilePageLocators
     recovery = RecoveryPageLocators
+
 
 
 __all__ = [
